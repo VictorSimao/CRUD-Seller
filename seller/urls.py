1398dfebcecfaa284ac7bb2c5e85e5582ccebacf
Alessandro Jeferson Carvalho <alessandro.carvalho@olist.com>
2021-06-09 10:23:55 -0300

    resolução do problema com git

--- seller/urls.py
+++ seller/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from sellerapp.views import home, index, form, create, view, edit, update,\
+    delete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('index', index, name='index'),
+    path('form/', form, name='form'),
+    path('create/', create, name='create'),
+    path('view/<int:pk>/', view, name='view'),
+    path('edit/<int:pk>/', edit, name='edit'),
+    path('update/<int:pk>/', update, name='update'),
+    path('delete/<int:pk>/', delete, name='delete'),
 ]
